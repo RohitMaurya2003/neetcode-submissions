39694d29180499a56bcc8d46fb13066282aee7e0
Maurya Rohit Krishnachand <168885708+RohitMaurya2003@users.noreply.github.com>
2026-07-26 19:57:52 +0530

    Add: baseball-game - submission-1

---
+++ Data Structures & Algorithms/baseball-game/submission-1.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        Deque<Integer> st = new ArrayDeque<>();
+
+        for (String c : operations) {
+           
+              if (c.equals( "+")) {
+                int temp1 = st.pop();
+                int temp2 = st.pop();
+                int sum = temp1 + temp2;
+                st.push(temp2);
+                st.push(temp1);
+                st.push(sum);
+
+            } else if (c.equals("D")) {
+                int temp1 = st.pop();
+                int d = temp1 * 2;
+                st.push(temp1);
+                st.push(d);
+            } else if (c.equals("C")) {
+                st.pop();
+            }
+            else{
+                st.push(Integer.parseInt(c));
+            }
+        }
+        int sum = 0;
+        while (!st.isEmpty()) {
+            sum += st.pop();
+        }
+        return sum;
+    }
+}
